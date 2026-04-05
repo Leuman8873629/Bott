c11376f7943e49e02eb328ce11aa13843a688389
Leuman8873629 <152858556+Leuman8873629@users.noreply.github.com>
2026-04-05 14:45:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -42,8 +42,8 @@ function createBot() {
   }
 
   bot = mineflayer.createBot({
-    host: "Tomanreturns.aternos.me",
-    port: 37089,
+    host: "TSLifestealsmp.aternos.me",
+    port: 27900,
     username: "heheh_botwaa",
     version: false,
     plugins: [AutoAuth, pvp, armorManager, pathfinder],
