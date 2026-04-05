dcdfd24d55fd9b37d9de2ff09381710a5ede2b03
Leuman8873629 <152858556+Leuman8873629@users.noreply.github.com>
2026-04-05 11:05:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ function createBot() {
     host: "Tomanreturns.aternos.me",
     port: 37089,
     username: "heheh_botwaa",
-    version: false,
+    version: 1.21.11,
     plugins: [AutoAuth],
     AutoAuth: {
       password: "bot112022",
